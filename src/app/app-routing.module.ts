f5910729cb495ec7f0558f42e6534ae20ca41c01
Stanislav Semenchuk <undefy23@gmail.com>
2023-03-31 17:15:37 +0300

    refactor: split views and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WelcomeComponent } from './components/welcome/welcome.component';
+import { WelcomeComponent } from './views/welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
